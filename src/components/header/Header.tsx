9511e7cc2b2ab55519229d309d9d2e43dad9c68b
gchme <gchmel@exadel.com>
2023-04-30 17:17:56 +0300

    added importing profile data from sample LInkedIn API response, made the profile a link to the linkedIn profile. Changed the header font and style, again. Made projects be actual elements and not just text

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import pfp from "../../assets/IMG_7517_2.jpg";
+import LinkedInApiResponse from "../../data/sampleData/linkedInApiResponseExample.json";
 
 interface profileData {
     pfp: string,
@@ -13,19 +14,24 @@ export default function Header() {
 
     function getCredentials(): profileData {
         let profile: profileData = {pfp: "", name: "", link: "", title: ""}
-        profile.name = "George Chmel"
+        profile.name = LinkedInApiResponse.localizedFirstName + " " + LinkedInApiResponse.localizedLastName
         profile.pfp = pfp
-        profile.link = ""
-        profile.title = "Software Engineer"
+        profile.link = `https://www.linkedin.com/in/${LinkedInApiResponse.vanityName}-${LinkedInApiResponse.id}/`
+        profile.title = LinkedInApiResponse.localizedHeadline
         return profile
     }
 
     let profile: profileData = getCredentials()
 
+    function redirectToLinkedIn() {
+        const url = profile.link
+        window.open(url, '_blank');
+    }
+
   return (
     <header>
         <img src={profile.pfp} className="header--pfp" alt="pfp"/>
-        <div className="header--name-n-title">
+        <div className="header--name-n-title" onDoubleClick={redirectToLinkedIn}>
             <h3 className="header--name">{profile.name}</h3>
             <p className="header--job">{profile.title}</p>
         </div>
--- src/components/myProjects/Project/Project.tsx
+++ src/components/myProjects/Project/Project.tsx
@@ -3,17 +3,21 @@ import React from 'react';
 import './Project.css';
 
 interface ProjectProps {
-    props: { img: string, name: string, link: string, id: number }
+    props: { img: string, name: string, link: string, id: number, description: string}
 }
 
 export default function Project({props}: ProjectProps) {
 
     const text: string = props.toString()
 
+    const realImg = props.img ? props.img : "https://via.placeholder.com/150"
+
     return (
-        <div>
-            <h3>Project</h3>
-            <p>{text}</p>
+        <div className="project">
+            <h3>Project {props.name}</h3>
+            <img src={realImg} alt="project"/>
+
+            <p>{props.description}</p>
         </div>
     )
 }
